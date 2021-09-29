68d0ec28ee6f213e64e9ddb3258d9ab054d03b22
David Dung <david.ken.dung@gmail.com>
2021-09-29 13:21:41 -0700

    ImageUploadUI comments and small changes

--- src/View/ImageUploadUI.java
+++ src/View/ImageUploadUI.java
@@ -1,9 +1,14 @@
 package View;
 
 import javafx.application.Application;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 public class ImageUploadUI extends Application {
+
+    // GraphicsContext of the main canvas
+    GraphicsContext gc;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -11,6 +16,18 @@ public class ImageUploadUI extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Image upload");
+
+        // TODO: create a canvas
+        // TODO: add a button that opens a file browser and adds the paths to some collection
+        // TODO: draw images on canvas
+        /*
+         * Eventually have something like
+         *
+         * for (Picture pic: pictures) {
+         *      gc.drawImage(pic.getImage(), x, y);
+         * }
+         *
+         */
 
     }
 }
