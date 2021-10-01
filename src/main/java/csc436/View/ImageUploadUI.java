514da40977a0fa18113202041ff11ae63ab65c55
David Dung <david.ken.dung@gmail.com>
2021-09-30 20:28:45 -0700

    Changes by clairemarie923

--- src/main/java/csc436/View/ImageUploadUI.java
+++ src/main/java/csc436/View/ImageUploadUI.java
@@ -52,6 +52,72 @@ public class ImageUploadUI extends Application {
          *
          */
 
+        Button importButt =  new Button("Import Images");
+        EventHandler<ActionEvent> importEvent = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                File file = doFilePickerUI();
+                if (file != null) {
+                    //deal with the file...
+                    //TODO: add a name this picture popup
+                    Picture pic = new Picture(file.getPath());
+
+                    //Show the uploaded image in a pop-up
+                    final Stage imageCropUI = new Stage();
+                    imageCropUI.initModality(Modality.WINDOW_MODAL); //this makes the rest of the application wait
+                    imageCropUI.initOwner(stage);
+
+                    EventHandler<WindowEvent> closeEvent = new EventHandler<WindowEvent>() {
+                        @Override
+                        public void handle(WindowEvent windowEvent){
+
+
+                            //Draw all the Pictures currently in the list!
+                            int y = 0;
+                            int x = 0;
+                            for (Picture p : list) {
+                                gc.drawImage(p.getWritableImage(), x * Picture.IMAGE_CROP_SIZE, y * Picture.IMAGE_CROP_SIZE);
+                                x++;
+                                if (x == 5) {
+                                    y ++;
+                                    x = 0;
+                                }
+                            } // for end
+                            pane.setCenter(canvas);
+                        }
+                    };
+
+                    imageCropUI.setOnCloseRequest(closeEvent);
+
+                    Canvas cropCanvas = new Canvas (200, 200);
+                    GraphicsContext imageGC = cropCanvas.getGraphicsContext2D();
+
+                    pic.getImageView().setFitHeight(75);
+                    pic.getImageView().setPreserveRatio(true);
+
+                    imageGC.drawImage(pic.getImage(), 0, 0, 200, 200);
+
+                    //TODO: can we make a movable, scaleable rectanlge, and a button to crop?
+
+                    double max = Math.max(pic.getWidth(), pic.getHeight());
+                    Rectangle2D area = new Rectangle2D(0, 0, Picture.IMAGE_CROP_SIZE, Picture.IMAGE_CROP_SIZE);
+
+                    BorderPane pain = new BorderPane();
+                    pain.setCenter(cropCanvas);
+
+                    Scene imageCropScene = new Scene(pain, 200, 200);
+                    imageCropUI.setScene(imageCropScene);
+                    imageCropUI.show();
+
+                    //crop image to square (note: gc.drawImage will auto resize)
+                    pic.createWriteableImage(area);
+                    list.add(pic);
+
+//                    imageGC.drawImage(pic.getWritableImage(), Picture.IMAGE_CROP_SIZE, Picture.IMAGE_CROP_SIZE);
+                }
+            }
+        };
+
         importButt.setOnAction(importEvent);
 
         pane.setTop(importButt);
@@ -61,5 +127,17 @@ public class ImageUploadUI extends Application {
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    public File doFilePickerUI() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Pick an image to upload");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+        //get the file chosen from the file picker
+        return fileChooser.showOpenDialog(stage);
     }
 }
