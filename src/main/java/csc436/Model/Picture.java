f8a8c6a18e95416b62daed4e18e943f5ad26e929
Clairegetz <clairegetz@email.arizona.edu>
2021-11-07 22:23:29 -0700

    Sprint 3 changes: #47: allow crop scrolling in all directions #48 Image upload could go out of bounds fixed also #53 naming photos and #54 sorting photos

--- src/main/java/csc436/Model/Picture.java
+++ src/main/java/csc436/Model/Picture.java
@@ -9,30 +9,24 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.Serializable;
 
-public class Picture implements Serializable{
+public class Picture implements Serializable, Comparable {
 
     public static final int IMAGE_CROP_SIZE = 75;
+    public PictureSortFlag sortMethod = PictureSortFlag.ByName;
 
     private String path;
     private String name;
+    private long timeIn;
+    private int bad;
     private double width, height; //actual image information
     private double x1, y1;  //crop parameters (set by user)
     private double cropDemensions;
-
-    /**
-     * Picture(String path, String name) constructor
-     * @param path - the file path of the picture
-     * @param name - the given name of the picture
-     */
-    public Picture(String path, String name) {
-        this.path = path;
-        this.name = name;
-        createImage();
-    } // Constructor end
 
     /**
      * Picture(String path, String name) alternative constructor (without name)
@@ -41,6 +35,7 @@ public class Picture implements Serializable{
     public Picture(String path) {
         this.path = path;
         this.name = path;
+        timeIn = System.currentTimeMillis();
         Image image = null;
         try {
             image = new Image(new FileInputStream(path));
@@ -54,6 +49,7 @@ public class Picture implements Serializable{
     public Picture(String path, double width, double height) {
         this.path = path;
         this.name = path;
+        timeIn = System.currentTimeMillis();
         Image image = null;
         try {
             image = new Image(new FileInputStream(path), width, height, true, true);
@@ -96,12 +92,25 @@ public class Picture implements Serializable{
         return view;
     }
 
+    public void setSortMethod(PictureSortFlag option) {
+        this.sortMethod = option;
+    }
+
     public void setSquareCrop(double x1, double y1) {
         this.x1 = x1;
         this.y1 = y1;
     }
 
     public void setCropDemensions(double dem) {
+        if(dem == 0) {
+            setSquareCrop(0, 0);
+            if(width < height) {
+                this.cropDemensions = width;
+            } else {
+                this.cropDemensions = height;
+            }
+            return;
+        }
         this.cropDemensions = dem;
     }
 
@@ -109,20 +118,34 @@ public class Picture implements Serializable{
         return cropDemensions;
     }
 
+    /**
+     * Getter and Setter Methods
+     */
+    public String getPath() { return path; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name;}
+    public long getUploadTime() { return timeIn; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+    public String toString() { return this.path; }
     public double getCropX() {
         return x1;
     }
-
     public double getCropY() {
         return y1;
     }
 
-    /**
-     * Getter Methods
-     */
-    public String getPath() { return path; }
-    public String getName() { return name; }
-    public double getWidth() { return width; }
-    public double getHeight() { return height; }
-    public String toString() { return this.path; }
+    @Override
+    public int compareTo(@NotNull Object o) {
+        int retVal = 0;
+        switch (sortMethod) {
+            case ByDate:
+                retVal = ((Long)((Picture)o).getUploadTime()).compareTo(timeIn);
+                break;
+            case ByName:
+                retVal = ((Picture)o).getName().compareTo(name);
+                break;
+        }
+        return retVal;
+    }
 }
--- src/main/java/csc436/View/ImageUploadUI.java
+++ src/main/java/csc436/View/ImageUploadUI.java
@@ -8,7 +8,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -17,17 +19,26 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.*;
+import javafx.util.Duration;
 import org.kordamp.bootstrapfx.scene.layout.Panel;
 
+import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class ImageUploadUI extends Application {
 
     public static final String TEST_FILE = "fileName.ser";
+    public static final int TOOLTIP_XOFFSET = 10;
+    public static final int TOOLTIP_YOFFSET = 7;
+
+    public static double cropOriginX;
+    public static double cropOriginY;
 
     private Stage stage;
     private ArrayList<Picture> list = new ArrayList<>();
+    private double dragStartX, dragStartY;
 
     BorderPane pane;
     GridPane grid;
@@ -79,8 +90,8 @@ public class ImageUploadUI extends Application {
                 }
             }
         };
-
         importButt.setOnAction(importEvent);
+
         BorderPane secondPane = new BorderPane();
         secondPane.setPadding(new Insets(10, 5, 5, 5));
         secondPane.setCenter(importButt);
@@ -96,14 +107,43 @@ public class ImageUploadUI extends Application {
     public void doCropUI(Picture pic, Boolean edit) {
         //temporary picture copy for editing
         Picture savedPicState = pic;
+        pic.setCropDemensions(0);
         //Show the uploaded image in a pop-up
         final Stage imageCropUI = new Stage();
+        imageCropUI.setTitle("Crop image");
         imageCropUI.initModality(Modality.WINDOW_MODAL); //this makes the rest of the application wait
         imageCropUI.initOwner(stage);
+
+        BorderPane imagePain = new BorderPane();
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.CENTER_RIGHT);
+        Button save_butt = new Button("Save");
+        Button cancel_butt = new Button("Cancel");
+        buttons.getChildren().addAll(save_butt, cancel_butt);
+        //imagePain.setBottom(buttons);
+
+        Group imageGroup = new Group(); //allows drawing on top of eachother
+        imagePain.setCenter(imageGroup);
+        ImageView img = new ImageView(pic.createImage());
+        imageGroup.getChildren().add(img);
+        Rectangle crop = new Rectangle(0,0,0,0);
+        //crop.widthProperty().bind(imagePain.widthProperty());
+
+        crop.setStroke(Color.CADETBLUE);
+        crop.setFill(null);
+        imageGroup.getChildren().add(crop);
 
         EventHandler<ActionEvent> saveEvent = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent){
+                if(crop.getHeight() > 0) {
+                    if(!checkCrop(pic, crop)) {
+                        pic.setCropDemensions(0);
+                    } else {
+                        pic.setSquareCrop(crop.getX(), crop.getY());
+                        pic.setCropDemensions(crop.getHeight());
+                    }
+                }
                 if (!edit) {
                     list.add(pic);
                 }
@@ -112,6 +152,7 @@ public class ImageUploadUI extends Application {
                 imageCropUI.close();
             }
         };
+        save_butt.setOnAction(saveEvent);
 
         EventHandler<ActionEvent> cancelEvent = new EventHandler<ActionEvent>() {
             @Override
@@ -123,60 +164,89 @@ public class ImageUploadUI extends Application {
                 imageCropUI.close();
             }
         };
-
-        BorderPane imagePain = new BorderPane();
-        HBox buttons = new HBox();
-        buttons.setAlignment(Pos.CENTER_RIGHT);
-        Button save_butt = new Button("Save");
-        save_butt.setOnAction(saveEvent);
-        Button cancel_butt = new Button("Cancel");
         cancel_butt.setOnAction(cancelEvent);
-        buttons.getChildren().addAll(save_butt, cancel_butt);
-        imagePain.setBottom(buttons);
-
-        Group imageGroup = new Group(); //allows drawing on top of eachother
-        imagePain.setCenter(imageGroup);
-        imageGroup.getChildren().add(new ImageView(pic.createImage()));
-        Rectangle crop = new Rectangle(0,0,0,0);
-        crop.setStroke(Color.CADETBLUE);
-        crop.setFill(null);
-        imageGroup.getChildren().add(crop);
 
         EventHandler<MouseEvent> begin_drag = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent click) {
+                if(click.getY() >= pic.getHeight() || click.getX() >= pic.getWidth()) {
+                    return;
+                }
                 imageGroup.getChildren().remove(crop);
-                pic.setSquareCrop(click.getX(), click.getY());
                 crop.setX(click.getX());
                 crop.setY(click.getY());
+                cropOriginX = click.getX();
+                cropOriginY = click.getY();
                 crop.setHeight(0);
                 crop.setWidth(0);
                 imageGroup.getChildren().add(crop);
             }
         };
+        imagePain.setOnMousePressed(begin_drag);
 
         EventHandler<MouseEvent> do_drag = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent drag) {
-                double x_drag = drag.getX() - pic.getCropX();
-                double y_drag = drag.getY() - pic.getCropY();
-                if (x_drag > y_drag) {
-                    pic.setCropDemensions(x_drag);
-                } else {
-                    pic.setCropDemensions(y_drag);
+                if(drag.getX() >= pic.getWidth() || drag.getY() >= pic.getHeight() || drag.getX() <= 0 || drag.getY() <= 0) {
+                    return;
                 }
-                crop.setWidth(pic.getCropDemensions());
-                crop.setHeight(pic.getCropDemensions());
+                double drag_width = drag.getX() - cropOriginX;
+                double drag_height = drag.getY() - cropOriginY;
+                if(drag_width < 0 && drag_height < 0) {
+                    System.out.print("NW: ");
+                    System.out.println(drag_width);
+                    System.out.println(drag_height);
+                    drag_width = drag_width * -1;
+                    drag_height= drag_height * -1;
+                    if (drag_width < drag_height) {
+                        crop.setX(drag.getX());
+                        crop.setY(cropOriginY - (cropOriginX - drag.getX()));
+                    } else {
+                        crop.setX(cropOriginX - (cropOriginY - drag.getY()));
+                        crop.setY(drag.getY());
+                    }
+                } else if(drag_width < 0) {
+                    System.out.println("SW");
+                    crop.setY(cropOriginY);
+                    drag_width = drag_width * -1;
+                    if (drag_width < drag_height ) {
+                        crop.setX(drag.getX());
+                    } else {
+                        crop.setX(cropOriginX - (drag.getY() - cropOriginY));
+                    }
+                } else if (drag_height < 0) {
+                    System.out.println("NE");
+                    crop.setX(cropOriginX);
+                    drag_height = drag_height * -1;
+                    if (drag_width < drag_height ) {
+                        crop.setY(cropOriginY - (drag.getX() - cropOriginX));
+                    } else {
+                        crop.setY(drag.getY());
+                    }
+                } else {
+                    System.out.println("SE");
+                    crop.setX(cropOriginX);
+                    crop.setY(cropOriginY);
+                }
+                if (drag_width < drag_height ) {
+                    crop.setWidth(drag_width);
+                    crop.setHeight(drag_width);
+                } else {
+                    crop.setWidth(drag_height);
+                    crop.setHeight(drag_height);
+                }
             }
         };
-
-        imagePain.setOnMousePressed(begin_drag);
         imagePain.setOnMouseDragged(do_drag);
 
         BorderPane pain = new BorderPane();
         pain.setCenter(imagePain);
+        pain.setBottom(buttons);
 
-        Scene imageCropScene = new Scene(pain, pic.getWidth(), (pic.getHeight() + 50));
+        imageCropUI.setMaxWidth(pic.getWidth());
+        imageCropUI.setMaxHeight(pic.getHeight() + 75);
+        imageCropUI.setResizable(false);
+        Scene imageCropScene = new Scene(pain, pic.getWidth(), (pic.getHeight() + 75));
         imageCropUI.setScene(imageCropScene);
         imageCropUI.show();
     }
@@ -192,9 +262,44 @@ public class ImageUploadUI extends Application {
                     doCropUI(p, true);
                 }
             };
+            EventHandler<ActionEvent> renamePicture = new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    TextInputDialog renamePopup = new TextInputDialog(p.getName());
+                    renamePopup.setTitle("Rename image");
+                    renamePopup.setHeaderText(null);
+                    renamePopup.setGraphic(null);
+                    Optional<String> result = renamePopup.showAndWait();
+                    if (result.isPresent()) {
+                        p.setName(renamePopup.getEditor().getText());
+                        drawPictures(pane, grid);  //refesh UI
+                        savePictures(list, TEST_FILE);  //save image
+                    }
+                }
+            };
+
+            ContextMenu menu = new ContextMenu();
+            MenuItem editContext = new MenuItem("Edit");
+            editContext.setOnAction(editPicture);
+            MenuItem renameContext = new MenuItem("Rename");
+            renameContext.setOnAction(renamePicture);
+            menu.getItems().addAll(editContext, renameContext);
+
             Button picButt = new Button();
             picButt.setGraphic(p.getCroppedImage());
             picButt.setOnAction(editPicture);
+            picButt.setContextMenu(menu);
+
+            Tooltip tip = new Tooltip(p.getName());
+            Tooltip.install(picButt, tip);
+            tip.setHideDelay(null);
+            picButt.setOnMouseMoved( ev -> {
+                tip.show(picButt, ev.getScreenX() + TOOLTIP_XOFFSET, ev.getScreenY() + TOOLTIP_YOFFSET);
+            });
+            picButt.setOnMouseExited( ev -> {
+                tip.hide();
+            });
+
             grid.add(picButt, x, y);
             x++;
             if (x == 5) {
@@ -232,6 +337,16 @@ public class ImageUploadUI extends Application {
         }
     }
 
+    private boolean checkCrop(Picture pic, Rectangle crop) {
+        if (crop.getX() > pic.getWidth()) {
+            return false;
+        }
+        if (crop.getY() > pic.getHeight()) {
+            return false;
+        }
+        return true;
+    }
+
     public ArrayList<Picture> loadPictures(String filename) {
         try {
             FileInputStream fileIn = new FileInputStream(filename);
@@ -243,10 +358,17 @@ public class ImageUploadUI extends Application {
         } catch (FileNotFoundException e) {
             System.out.println("No picture file found");
             return new ArrayList<>();
-        } catch (IOException e2) {
-            e2.printStackTrace();
-        } catch (ClassNotFoundException e3) {
+        } catch (InvalidClassException e2) {
+            System.out.println("Class Mismatch... change in Picture.java?");
+            File file = new File(filename);
+            if(!file.delete()) {
+                System.out.println("Failed to delete the bad file... manual delete required");
+            }
+            return new ArrayList<>();
+        } catch ( IOException e3) {
             e3.printStackTrace();
+        } catch (ClassNotFoundException e4) {
+            e4.printStackTrace();
         }
         return null;
     }
